2977f7f364c69f62390647a057686ae40b9102d0
agimpel <agimpel@student.ethz.ch>
2018-02-11 22:39:05 +0100

    first draft of webhook receiver and mailer

---
+++ docroot/webhook.php
@@ -0,0 +1,91 @@
+<?php
+
+// the shared secret, used to sign the POST data (using HMAC with SHA1)
+// Not documented anywhere, generate random password with high entropy and set as environment variable on server via 'export github_webhook_secret=<password>'
+// Remember to also change the webhook secret in Github
+$secret = getenv('github_webhook_secret');
+
+
+// where to log errors and successful requests
+define('LOGFILE', 'webhook.log');
+
+
+// receive POST data for signature calculation
+$post_data = file_get_contents('php://input');
+$signature = hash_hmac('sha1', $post_data, $secret);
+
+// required data in POST body
+$required_data = array(
+	'ref' => 'refs/heads/live',
+	'repository' => array(
+		'name' => 'chemtogether-web',
+	),
+);
+
+// required data in headers
+$required_headers = array(
+  'REQUEST_METHOD' => 'POST',
+  'HTTP_X_GITHUB_EVENT' => 'push',
+  'HTTP_USER_AGENT' => 'GitHub-Hookshot/*',
+  'HTTP_X_HUB_SIGNATURE' => 'sha1='.$signature,
+);
+
+
+
+error_reporting(0);
+
+function log_msg($msg) {
+  if(LOGFILE != '') {
+    file_put_contents(LOGFILE, $msg . "\n", FILE_APPEND);
+  }
+}
+
+function match_header($have, $should, $name = 'array') {
+  $return = true;
+
+  if(is_array($have)) {
+    foreach($should as $key => $value) {
+      if(!array_key_exists($key, $have)) {
+        log_msg("Missing: $key");
+        $return = false;
+      }
+      else if(is_array($value) && is_array($have[$key])) {
+        $return &= array_matches($have[$key], $value);
+      }
+      else if(is_array($value) || is_array($have[$key])) {
+        log_msg("Type mismatch: $key");
+        $return = false;
+      }
+      else if(!fnmatch($value, $have[$key])) {
+        log_msg("Failed comparison: $key={$have[$key]} (expected $value)");
+        $return = false;
+      }
+    }
+  }
+  else {
+    log_msg("Not an array: $name");
+    $return = false;
+  }
+  return $return;
+}
+
+
+log_msg("\n\n=== Received request from {$_SERVER['REMOTE_ADDR']} ===");
+header("Content-Type: text/plain");
+
+$data = json_decode($post_data, true);
+
+// First do all checks and then report back in order to avoid timing attacks
+$headers_ok = array_matches($_SERVER, $required_headers, '$_SERVER');
+$data_ok = array_matches($data, $required_data, '$data');
+
+if($headers_ok && $data_ok) {
+  log_msg('Request successful.');
+  // execute git script
+} else {
+  log_msg('Request denied.');
+  http_response_code(403);
+  die("Forbidden\n");
+}
+
+?>
---
+++ server/mailer.php
@@ -0,0 +1,48 @@
+<?php
+
+//Import PHPMailer classes into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/Exception.php';
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+//Create a new PHPMailer instance
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "";
+//Password to use for SMTP authentication
+$mail->Password = "";
+//Set who the message is to be sent from
+$mail->setFrom('', 'First Last');
+//Set an alternative reply-to address
+// $mail->addReplyTo('replyto@example.com', 'First Last');
+//Set who the message is to be sent to
+$mail->addAddress('', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+$mail->Body = 'Test';
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+    echo "Message sent!";
+}
