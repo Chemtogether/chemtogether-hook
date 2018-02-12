a2e1a28ba4e1692cb36346bff2e82ee5016c9d4f
agimpel <agimpel@student.ethz.ch>
2018-02-12 22:10:44 +0100

    downgrade to PHPMailer 5.2-stable due to version restriction

--- server/mail-service.php
+++ server/mail-service.php
@@ -1,12 +1,12 @@
 <?php
-
-//Import PHPMailer classes into the global namespace
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'phpmailer/Exception.php';
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
+/**
+ * This example shows settings to use when sending via Google's Gmail servers.
+ * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
+ */
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
+require 'phpmailer/PHPMailerAutoload.php';
 //Create a new PHPMailer instance
 $mail = new PHPMailer;
 //Tell PHPMailer to use SMTP
@@ -16,6 +16,8 @@ $mail->isSMTP();
 // 1 = client messages
 // 2 = client and server messages
 $mail->SMTPDebug = 2;
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
 //Set the hostname of the mail server
 $mail->Host = 'smtp.gmail.com';
 // use
@@ -32,17 +34,20 @@ $mail->Username = "";
 //Password to use for SMTP authentication
 $mail->Password = "";
 //Set who the message is to be sent from
-$mail->setFrom('', 'First Last');
+$mail->setFrom('', 'Webhook Chemtogether');
 //Set an alternative reply-to address
-// $mail->addReplyTo('replyto@example.com', 'First Last');
+$mail->addReplyTo('webmaster@chemtogether.ethz.ch', 'Webmaster Chemtogether');
 //Set who the message is to be sent to
 $mail->addAddress('', 'John Doe');
 //Set the subject line
 $mail->Subject = 'PHPMailer GMail SMTP test';
-$mail->Body = 'Test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->Body = "Test";
 //send the message, check for errors
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
     echo "Message sent!";
 }
+?>
